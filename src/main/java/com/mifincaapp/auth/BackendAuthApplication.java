1f04a583122e66c663dbed7dcaedb4214f826eb1
gabrielrubio22 <gabrielrubioduque22@gmail.com>
2025-05-17 20:13:34 -0500

    feat(config): detectar entorno local o producción y cargar .env según corresponda

--- src/main/java/com/mifincaapp/auth/BackendAuthApplication.java
+++ src/main/java/com/mifincaapp/auth/BackendAuthApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendAuthApplication{
 
     public static void main(String[] args) {
-        
-        Dotenv dotenv = Dotenv.configure().load();
-        // Establece las variables como propiedades del sistema
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());  // Configura las variables como propiedades del sistema
-            System.out.println(entry.getKey() + "=" + entry.getValue());  // Muestra las variables cargadas
-        });
+        //Detecta el entorno, desarrollo o producción
+        String envType = System.getenv("ENV_TYPE");
+        if("local".equalsIgnoreCase(envType)){
+            Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+        }else {
+            System.out.println("No cargando .env, asumiendo entorno producción");
+        }
 
         SpringApplication.run(BackendAuthApplication.class, args);
         
